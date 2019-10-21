1ab0c86df955209484c6b91cfeee2be14b75c0da
BojteTamas <bojtetomi92@gmail.com>
2019-10-21 19:42:54 +0300

    Troll,

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,4 +5,47 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+    public static void main(String[] args) {
+    public static void main(String[] args) {
+    public static void main(String[] args) {
+    public static void main(String[] args) {
+        System.out.println("Hello, Worsadsald!");
+    }
+}
+blic class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    public static void main(String[] args) {
+    public static void main(String[] args) {
+    public static void main(Sring[] args) {
+        System.out.println("Hello, World!");
+    }
+    public static void main(String[] args) {
+    public static void main(String[] args) {
+    public static void main(String[] args) {
+    public static void main(String[] args) {
+        System.out.println("Hello, Worsadsald!");
+    }
+}
+blic class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    public static void main(String[] args) {
+    public static void main(String[] args) {
+    public static void main(String[] args) {
+    public static void main(String[] args) {
+        System.out.println("Hello, Worsadsald!");
+    }
+}
+
+    public static void main(String[] args) {
+    public static void main(String[] args) {
+    public static void main(String[] args) {
+    public static void main(String[] args) {
+        System.out.println("Hello, Worsadsald!");
+    }
 }
